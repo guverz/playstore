1718718a4d123b44becfbcffc5cdd7b9246146de
guverz <guverz1994@mail.ru>
2024-11-11 16:36:46 +0800

    created class csvCats

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
+import java.util.Locale.Category
+
 fun main() {
     
 }
+
+enum class Type {
+    FREE, PAID
+}
+
+enum class ContentRating {
+    EVERYONE, TEEN, MATURE 17+, ADULT
+}
+
+data class csvCats(
+    val name: String,
+    val category: String,
+    val rating: Float,
+    val reviews: Int,
+    val size: String,
+    val installs: String,
+    val type: Type,
+    val price: Double,
+    val contentRating: ContentRating,
+    val genres: String,
+    val lastUpdate: Date,
+    val currentVer: String,
+    var androidVer: String,
+)
